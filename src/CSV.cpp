7e95758ae23671d77397893bca9fdb51f597faec
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2020-03-13 23:01:29 +0100

    :pencil: i have commented some part in csv.cpp

--- src/CSV.cpp
+++ src/CSV.cpp
@@ -2,7 +2,7 @@
 * @Author: Baptiste Bertrand-Rapello
 * @Date:   2020-03-11 16:51:17
 * @Last Modified by:   Baptiste Bertrand-Rapello
-* @Last Modified time: 2020-03-13 16:58:07
+* @Last Modified time: 2020-03-13 22:25:53
 */
 
 #include "../includes/CSV.hh"
@@ -28,17 +28,27 @@ CSV::~CSV()
 
 }
 
-
+/* everythning needed for the creation of the file
+** if you have to prepare the header or something else
+*/
 void CSV::createTheFile()
 {
 	std::cout << "\x1B[35mDans la methode de CSV : createTheFile\x1B[0m" << std::endl;
 }
 
+
+/*
+** here where you write the data in the file
+*/
 void CSV::writeTheFile()
 {
 	std::cout << "\x1B[35mDans la methode de CSV : writeTheFile\x1B[0m" << std::endl;
 }
 
+/*
+** here where you collect the data from the file, 
+** where you READ it
+*/
 void CSV::getThePicture(std::ifstream & inFile, unsigned int)
 {
 	int length = 25;
@@ -53,11 +63,16 @@ void CSV::getThePicture(std::ifstream & inFile, unsigned int)
 	std::cout << CYAN << "le une ligne\n" << RST << line << std::endl;
 }
 
-void CSV::getPicture(char *, unsigned int)
+// i think a previous version of getThePicture ^^
+void CSV::getPicture(char fileInCharTab, unsigned int)
 {
 	std::cout << "\x1B[35mDans la methode de CSV : getPicture\x1B[0m" << std::endl;
 }
 
+/*
+** everything to do here concern when you collect the data from the file
+** so from file -> var in object 
+*/
 void CSV::setDataFromFile(unsigned int)
 {
 	std::cout << "\x1B[35mDans la methode de CSV : setDataFromFile\x1B[0m" << std::endl;
